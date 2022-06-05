b0a1ce8053c953d09044036fafc841c0bb3e87dc
Aibekbjj <aibekon@mail.ru>
2022-06-05 17:12:33 +0600

    Final version of the application

--- UFC fighters/Models/FighterModel.swift
+++ UFC fighters/Models/FighterModel.swift
@@ -27,11 +27,11 @@ struct FighterModel {
 
 extension FighterModel {
     static func getData() -> [FighterModel] {
-        return [FighterModel(name: "Shavkat Rakhmonov", wins: 15, defeats: 0, draws: 0, urlString: "Zj0lBsRKyJQ", photo: "photo_shavkat_rakhmonov"),
+        return [FighterModel(name: "Shavkat Rakhmonov", wins: 15, defeats: 0, draws: 0, urlString: "nyzvwROUn48", photo: "photo_shavkat_rakhmonov"),
                 FighterModel(name: "Islam Makhachev", wins: 21, defeats: 1, draws: 0, urlString: "iMzXPmWtMAw", photo: "photo_islam_makhachev"),
                 FighterModel(name: "Zubaira Tukhugov", wins: 20, defeats: 5, draws: 1, urlString: "kkKikxY7o7g", photo: "photo_zubaira_tukhugov"),
                 FighterModel(name: "Damir Ismagulov", wins: 20, defeats: 1, draws: 0, urlString: "JW5lF6Jbaa8", photo: "photo_damir_ismagulov"),
-                FighterModel(name: "Movsar Evloev", wins: 15, defeats: 0, draws: 0, urlString: "WC-EgPIjNZU", photo: "photo_movsar_evloev"),
+                FighterModel(name: "Movsar Evloev", wins: 16, defeats: 0, draws: 0, urlString: "WC-EgPIjNZU", photo: "photo_movsar_evloev"),
                 FighterModel(name: "Magomed Ankalaev", wins: 18, defeats: 1, draws: 0, urlString: "wyj4xADpUtQ", photo: "photo_magomed_ankalaev"),
                 FighterModel(name: "Abubakar Nurmagomedov", wins: 16, defeats: 3, draws: 1, urlString: "oAWRDccDaho", photo: "photo_abubakar_nurmagomedov"),
                 FighterModel(name: "Zabit Magomedsharipov", wins: 18, defeats: 1, draws: 0, urlString: "kKxsa_MoYmo", photo: "photo_zabit_magomedsharipov"),
